702e61a08e40acc2cb10fa09d2493dd7117e99c3
Leticia-Mendiola <lmendiola509@gmail.com>
2021-02-25 18:54:18 -0600

    set up second question and attempted to implement setDefault()

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -2,8 +2,8 @@ var timeEL = document.querySelector(".time");
 
 var secondsLeft=60
 
-var setTime = function() {
-    var timerInterval = setInterval(function(){
+var setTime = function(event) {
+        var timerInterval = setInterval(function(){
         secondsLeft --;
         timeEL.textContent = "time left" + secondsLeft;
 
@@ -21,19 +21,46 @@ function sendMessage(){
 setTime();
 
 var question1 = document.querySelector(".question1");
+var question2 = document.querySelector(".question2");
+var question3 = document.querySelector(".question3");
+var question4 = document.querySelector(".question4");
 var yes = document.querySelector(".yes");
 var no = document.querySelector(".no");
 
-yes.addEventListener("click", function() {
-    sendMessage();
-    function sendMessage() {
-    question1.textContent = "Correct!";
-    };
+question1.addEventListener("click", function(event) {
+    // event.preventDefault();
+    yes.addEventListener("click", function(event) {
+        event.preventDefault();
+        sendMessage();
+        function sendMessage() {
+        question1.textContent = "Correct!";
+        };
+    })
+
+    no.addEventListener("click", function(event) {
+        event.preventDefault();
+        sendMessage();
+        function sendMessage() {
+        question1.textContent = "Incorrect";
+        };
+    })
+    
 })
 
-no.addEventListener("click", function() {
-    sendMessage();
-    function sendMessage() {
-    question1.textContent = "Incorrect";
-    };
+question2.addEventListener("click", function() {
+    yes.addEventListener("click", function(event) {
+        event.preventDefault();
+        sendMessage();
+        function sendMessage() {
+        question1.textContent = "Correct!";
+        };
+    })
+
+    no.addEventListener("click", function(event) {
+        event.preventDefault();
+        sendMessage();
+        function sendMessage() {
+        question2.textContent = "Incorrect";
+        };
+    })
 })
