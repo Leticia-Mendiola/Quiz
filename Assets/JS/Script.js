1cf0e92055a5b260e2deba333456bc881133b0ce
Leticia-Mendiola <lmendiola509@gmail.com>
2021-02-26 11:09:21 -0600

    debuged setDefault(), added final questions, recoded JS to use event.targets and dataset.values

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,11 +1,9 @@
 var timeEL = document.querySelector(".time");
-
 var secondsLeft=60
-
 var setTime = function(event) {
         var timerInterval = setInterval(function(){
         secondsLeft --;
-        timeEL.textContent = "time left" + secondsLeft;
+        timeEL.textContent = "Time Left:" + secondsLeft;
 
         if(secondsLeft===0){
             clearInterval(timerInterval);
@@ -13,11 +11,9 @@ var setTime = function(event) {
         }
     }, 1000);
 }
-
 function sendMessage(){
     timeEL.textContent="TIME UP!";
 }
-
 setTime();
 
 var question1 = document.querySelector(".question1");
@@ -28,39 +24,57 @@ var yes = document.querySelector(".yes");
 var no = document.querySelector(".no");
 
 question1.addEventListener("click", function(event) {
-    // event.preventDefault();
-    yes.addEventListener("click", function(event) {
-        // event.preventDefault();
+    event.preventDefault();
+    var element = event.target
+    if (element.dataset.value="yes") {
         sendMessage();
         function sendMessage() {
-        question1.textContent = "Correct!";
-        };
-    })
-
-    no.addEventListener("click", function(event) {
-        // event.preventDefault();
+        question1.textContent = "Correct!";}
+    } else { if(element.dataset.value="no") {
         sendMessage();
         function sendMessage() {
-        question1.textContent = "Incorrect";
-        };
-    })
-    
+        question1.textContent = "Incorrect";}
+    }}
 })
 
-question2.addEventListener("click", function() {
-    yes.addEventListener("click", function(event) {
-        // event.preventDefault();
+question2.addEventListener("click", function(event) {
+    event.preventDefault();
+    var element = event.target;
+    if (element.dataset.value="yes") {
         sendMessage();
         function sendMessage() {
-        question2.textContent = "Correct!";
-        };
-    })
+        question2.textContent = "Correct!";}
+    } else { if(element.dataset.value="no") {
+        sendMessage();
+        function sendMessage() {
+        question2.textContent = "Incorrect";}
+    }}
+})
 
-    no.addEventListener("click", function(event) {
-        // event.preventDefault();
+question3.addEventListener("click", function(event) {
+    event.preventDefault();
+    var element = event.target;
+    if (element.dataset.value="yes") {
         sendMessage();
         function sendMessage() {
-        question2.textContent = "Incorrect";
-        };
-    })
+        question3.textContent = "Correct!";}
+    } else { if(element.dataset.value="no") {
+        sendMessage();
+        function sendMessage() {
+        question3.textContent = "Incorrect";}
+    }}
+})
+
+question4.addEventListener("click", function(event) {
+    event.preventDefault();
+    var element = event.target;
+    if (element.dataset.value="yes") {
+        sendMessage();
+        function sendMessage() {
+        question4.textContent = "Correct!";}
+    } else { if(element.dataset.value="no") {
+        sendMessage();
+        function sendMessage() {
+        question4.textContent = "Incorrect";}
+    }}
 })
